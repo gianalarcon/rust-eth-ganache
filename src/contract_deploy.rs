78ce2c33cf5fefdf065049e4cd44bbe5157075b5
gianmarcoalarcon <gian.alarcon@unmsm.edu.pe>
2023-03-07 17:14:09 +0800

    Feat: Add signer client, deploy contract, contractfactory, compile(), print_project()

--- src/contract_deploy.rs
+++ src/contract_deploy.rs
@@ -1,10 +1,11 @@
 use ethers::contract::Contract;
 use ethers::prelude::{
-  BlockNumber, ConfigurableArtifacts, ContractFactory, LocalWallet, Project, ProjectCompileOutput,
-  ProjectPathsConfig, Signer, SignerMiddleware, U256,
+  BlockNumber, Client, ConfigurableArtifacts, ContractFactory, LocalWallet, Project,
+  ProjectCompileOutput, ProjectPathsConfig, Signer, SignerMiddleware, U256,
 };
 use ethers::utils::Ganache;
 use ethers_providers::{Middleware, Provider};
+use ethers_solc::artifacts::bytecode;
 use ethers_solc::Artifact;
 use eyre::Result;
 use eyre::{eyre, ContextCompat};
@@ -36,7 +37,7 @@ async fn main() -> Result<()> {
   println!("Ganache started with chain_id: {}", chain_id);
   let project = compile("examples/").await?;
 
-  //print_project(project.clone()).await?;
+  print_project(project.clone()).await?;
 
   let balance = provider.get_balance(wallet.address(), None).await?;
   println!(
@@ -45,12 +46,90 @@ async fn main() -> Result<()> {
     balance
   );
 
-  pub async fn compile(root: &str) -> Result<ProjectCompileOutput<ConfigurableArtifacts>> {
-    todo!()
-  }
+  let contract_name = "BUSDImplementation";
+  let contract = project
+    .find(contract_name)
+    .context("Contract not found")?
+    .clone();
 
-  pub async fn print_project(Project: ProjectCompileOutput<ConfigurableArtifacts>) {
-    todo!()
+  let (abi, bytecode, _) = contract.into_parts();
+  let abi = abi.context("Missing abi from contract")?;
+  let bytecode = bytecode.context("Missing bycode from contract")?;
+  //Create signer client-
+  let wallet = wallet.with_chain_id(chain_id);
+  let client = SignerMiddleware::new(provider.clone(), wallet).into();
+  //Deploy contract
+  let factory = ContractFactory::new(abi.clone(), bytecode, client);
+  let mut deployer = factory.deploy(())?;
+  let block = provider
+    .clone()
+    .get_block(BlockNumber::Latest)
+    .await?
+    .context("Failed to get block")?;
+
+  let gas_price = block
+    .next_block_base_fee()
+    .context("Failed to get the base fee for the next block")?;
+  deployer.tx.set_gas_price::<U256>(gas_price);
+
+  let contract = deployer.clone().legacy().send().await?;
+  println!(
+    "BUSDImpl contract address {}",
+    contract.address().encode_hex::<String>()
+  );
+  Ok(())
+}
+
+pub async fn compile(root: &str) -> Result<ProjectCompileOutput<ConfigurableArtifacts>> {
+  let root = PathBuf::from(root);
+  if !root.exists() {
+    return Err(eyre!("Project root: {root:?} does not exist!"));
   }
-  todo!()
+  let paths = ProjectPathsConfig::builder()
+    .root(&root)
+    .sources(&root)
+    .build()?;
+
+  let project = Project::builder()
+    .paths(paths)
+    .set_auto_detect(true)
+    .no_artifacts()
+    .build()?;
+
+  let output = project.compile()?;
+  if output.has_compiler_errors() {
+    Err(eyre!(
+      "Compiling solidity project failed: {:?}",
+      output.output().errors
+    ))
+  } else {
+    Ok(output.clone())
+  }
+}
+
+pub async fn print_project(project: ProjectCompileOutput<ConfigurableArtifacts>) -> Result<()> {
+  let artifacts = project.into_artifacts();
+  for (id, artifact) in artifacts {
+    let name = id.name;
+    let abi = artifact.abi.context("No ABI found for artifact {name}")?;
+
+    println!("{}", "=".repeat(80));
+    println!("CONTRACT: {:?}", name);
+
+    let contract = &abi.abi;
+    let functions = contract.functions();
+    let functions = functions.cloned();
+    let constructor = contract.constructor();
+
+    if let Some(constructor) = constructor {
+      let args = &constructor.inputs;
+      println!("CONSTRUCTOR args: {args:?} ");
+    }
+    for func in functions {
+      let name = &func.name;
+      let params = &func.inputs;
+      println!("FUNCTION {name} {params:?}");
+    }
+  }
+  Ok(())
 }
